38992b68ee57ac6c4d1062e5fea7f6287bb7cdcf
mouhamed2213 <Mouhamedbk1002@gmail.com>
2025-09-22 00:02:46 +0000

    set up form / create post request

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,28 @@ import HomePage from './pages/HomePage';
 import Jobs from './pages/jobs';
 import Job from './pages/job';
 import MainLayout from './Layouts/MainLayout';
+import AddJobPage from './pages/AddJobPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-// delcare route elemnts
-const routes = createRoutesFromElements(
-  <>
-    {/* User Page Route  */}
-    <Route path="/" Component={MainLayout}>
-      <Route index Component={HomePage} />
-      <Route path="jobs" Component={Jobs} />
-      <Route path="job/:id" Component={Job} />
-
-      {/* Not found page  */}
-      <Route path="*" Component={NotFoundPage} />
-    </Route>
-  </>
-);
-// wrapp route elemnt
-const router = createBrowserRouter(routes);
-
 const App = () => {
+  // delcare route elemnts
+  const routes = createRoutesFromElements(
+    <>
+      {/* User Page Route  */}
+      <Route path="/" Component={MainLayout}>
+        <Route index Component={HomePage} />
+        <Route path="jobs" Component={Jobs} />
+        <Route path="job/:id" Component={Job} />
+        <Route path="add-job" element={<AddJobPage />} />
+
+        {/* Not found page  */}
+        <Route path="*" Component={NotFoundPage} />
+      </Route>
+    </>
+  );
+  // wrapp route elemnt
+  const router = createBrowserRouter(routes);
+
   return <RouterProvider router={router} />;
 };
 
